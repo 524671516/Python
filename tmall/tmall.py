010c10a01624a5e5a9e82100f10b2e78ded99b69
524671516 <524671516@qq.com>
2017-11-24 18:16:02 +0800

    121

--- tmall/tmall.py
+++ tmall/tmall.py
@@ -14,42 +14,45 @@ database="tmall"
 conn = pymssql.connect(server, user, password,database)
 cur=conn.cursor()
 
-for i in list(range(1,2)):
-    url = 'https://rate.tmall.com/list_detail_rate.htm?itemId=557193106901&spuId=869151019&sellerId=1613774608&order=1&currentPage=%s'%i
-    
-    request = urllib.request.Request(url=url,method='GET')
-    response = urllib.request.urlopen(request)
+def getAssessment(start,end):
+    i=1
+    flag = False
+    sql="delete from t_assessment where t_date<'"+end+"' and t_date>='"+start+"'"
+    cur.execute(sql)
+    conn.commit()
+    sum_count=0
+    while(flag==False):
+    #for i in list(range(1,10)):
+        single_count=0
+        url = 'https://rate.tmall.com/list_detail_rate.htm?itemId=41482176804&spuId=235557795&sellerId=1613774608&order=1&currentPage=%s'%i
+        request = urllib.request.Request(url=url,method='GET')
+        response = urllib.request.urlopen(request)
+        response_json = response.read()
+        str = bytes.decode(response_json,'gbk')
+        str = "{"+str.strip()+"}"
+        try:
+            assessment = json.loads(str)
+            print("no error:",i)
+            for item in assessment["rateDetail"]["rateList"]:
+                if(start<=item["rateDate"]<end):
+                     sql="insert into t_assessment(t_date,t_content,t_type) values ('"+item["rateDate"]+"','"+item["rateContent"]+"',0)"
+                     cur.execute(sql)
+                     conn.commit()
+                     single_count=single_count+1
+                     sum_count=sum_count+1
+                if(item["rateDate"]<start):
+                    flag = True
+                    break
+            i=i+1
+            print("single_count:",single_count)
+            print("sum_count:",sum_count)
+        except:
+            print("error:",i)
+            time.sleep(3)
+        
+        
 
-    response_json = response.read()
-    str = bytes.decode(response_json,'gbk')
-    str = "{"+str.strip()+"}"
-    #file =open('C:/Users/CAIWU02/Desktop/tmall.txt','a')
-    #file.write(str)
-    #file.close()
-    assessment = json.loads(str)
-    for item in assessment["rateDetail"]["rateList"]:
-        sql="insert into t_assessment(t_date,t_content,t_type) values ('"+item["rateDate"]+"','"+item["rateContent"]+"',0)"
-        #print(sql)
-        cur.execute(sql)
-        conn.commit()
-    #print(i)
-    # 循环抓取数据
-    #nickname = []
-    #ratecontent = []
-    #ratedate=[]
-    #content = requests.get(url).text
-    #nickname.extend(re.findall('"displayUserNick":"(.*?)"',content))
-    #ratedate.extend(re.findall('"rateDate":"(.*?)"',content))
-    #ratecontent.extend(re.findall('"rateContent":"(.*?)"',content))
-    #print(nickname,ratedate,ratecontent)
-    # 写入数据
-    # file =open('C:/Users/CAIWU02/Desktop/tmall.txt','a')
-    #for j in list(range(0,len(nickname))):
-    #    print(j)
-    #    #print()
-    #    sql="insert into t_assessment(t_date,t_content,t_type) values ('"+ratedate[j]+"','"+ratecontent[j]+"',0)"
-    #    # print(sql)
-    #    cur.execute(sql)
-    #    conn.commit()
-    #    #file.write(','.join((nickname[i],ratedate[i],ratecontent[i],'好评'))+'\n')
-    ##file.close()
+now = datetime.datetime.now()
+end = now.strftime('%Y-%m-%d 00:00:00')
+start = (now + datetime.timedelta(days=-1)).strftime('%Y-%m-%d 00:00:00')
+getAssessment(start,end)
